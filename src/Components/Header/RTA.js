d10d5bb19d14fbea4a9afee1eda2b43a89369d27
keerthanagiri2000 <keerthanagiri2000@gmail.com>
2022-09-12 18:04:48 +0530

    update resume

--- src/Components/Header/RTA.js
+++ src/Components/Header/RTA.js
@@ -4,7 +4,7 @@ import Resume from '../../Asserts/resume.pdf'
 const RTA = () => {
   return (
     <div className='rta'>
-        <a href={Resume} download className='btn'>Download Resume</a>
+        <a href='https://drive.google.com/file/d/14-9Mlah6TdC-jKi3XMOBzmkAXfa2qsAr/view?usp=sharing' target='_blank' rel="noopener noreferrer" className='btn'>Download Resume</a>
         <a href='#contact' className='btn btn-primary'>Let's Talk</a>
     </div>
   )
